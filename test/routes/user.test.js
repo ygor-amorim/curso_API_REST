787799b47ba2b27e174ff91295a7d90ec95e3f46
ygor-amorim <ygoramorim3@hotmail.com>
2022-08-25 23:08:39 +0100

    knex and postgre integration

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,22 +1,21 @@
 const request = require('supertest');
 
-const app = require('../src/app');
+const app = require('../../src/app');
 
 test('Deve listar todos os usuários', () => {
   return request(app).get('/users')
     .then((res) => {
       expect(res.status).toBe(200);
-      expect(res.body).toHaveLength(1);
-      expect(res.body[0]).toHaveProperty('name', 'Fulano');
+      expect(res.body.length).toBeGreaterThan(0);
     });
 });
 
 test('Deve inserir usuário com sucesso', () => {
+  const mail = `${Date.now()}@mail.com`;
   return request(app).post('/users')
-    .send({ name: 'Jane Doe', mail: 'janedoe@mail.com' })
+    .send({ name: 'Jane Doe', mail, passwd: '123456' })
     .then((res) => {
       expect(res.status).toBe(201);
       expect(res.body.name).toBe('Jane Doe');
-      expect(res.body.mail).toBe('janedoe@mail.com');
     });
 });
