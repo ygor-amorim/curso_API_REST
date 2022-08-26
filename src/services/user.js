2681eea5c08367002e51407923d8432a739004ff
ygor-amorim <ygoramorim3@hotmail.com>
2022-08-26 20:32:18 +0100

    added logging db querys and added service layer

---
+++ src/services/user.js
@@ -0,0 +1,10 @@
+module.exports = (app) => {
+  const findAll = () => {
+    return app.db('users').select();
+  };
+
+  const save = (user) => {
+    return app.db('users').insert(user, '*');
+  };
+  return { findAll, save };
+};
