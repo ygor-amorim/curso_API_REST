0ef91e9039795f6ccd8130006c4cd27d83bfd39e
ygor-amorim <ygoramorim3@hotmail.com>
2022-09-05 18:50:15 +0100

    accounts insert and listing commit

---
+++ src/routes/accounts.js
@@ -0,0 +1,15 @@
+module.exports = (app) => {
+  const create = (req, res) => {
+    app.services.account.save(req.body)
+      .then((result) => {
+        return res.status(201).json(result[0]);
+      });
+  };
+
+  const getAll = (req, res) => {
+    app.services.account.findAll()
+      .then((result) => res.status(200).json(result));
+  };
+
+  return { create, getAll };
+};
