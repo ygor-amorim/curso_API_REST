cc9af4d555df10d9233f4219255291d0581d0b3d
ygor-amorim <ygoramorim3@hotmail.com>
2022-09-06 22:48:11 +0100

    error managing initial commit

--- src/services/user.js
+++ src/services/user.js
@@ -1,15 +1,17 @@
+const ValidationError = require('../errors/ValidationError');
+
 module.exports = (app) => {
   const findAll = (filter = {}) => {
     return app.db('users').where(filter).select();
   };
 
   const save = async (user) => {
-    if (!user.name) return { error: 'Nome é atributo obrigatório' };
-    if (!user.mail) return { error: 'E-mail é um atributo obrigatório' };
-    if (!user.passwd) return { error: 'Senha é um atributo obrigatório' };
+    if (!user.name) throw new ValidationError('Nome é atributo obrigatório');
+    if (!user.mail) throw new ValidationError('E-mail é um atributo obrigatório');
+    if (!user.passwd) throw new ValidationError('Senha é um atributo obrigatório');
 
     const userDb = await findAll({ mail: user.mail });
-    if (userDb && userDb.length > 0) return { error: 'Já existe um usuário com esse email' };
+    if (userDb && userDb.length > 0) throw new ValidationError('Já existe um usuário com esse email');
 
     return app.db('users').insert(user, '*');
   };
