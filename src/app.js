590651c660bb1d685eee71162ac09bf763644707
ygor-amorim <ygoramorim3@hotmail.com>
2022-09-09 15:20:20 +0100

    generic error managing

--- src/app.js
+++ src/app.js
@@ -18,4 +18,11 @@ app.get('/', (req, res) => {
   res.status(200).send();
 });
 
+app.use((err, req, res, next) => {
+  const { name, message, stack } = err;
+  if (name === 'ValidationError') res.status(400).json({ error: message });
+  else res.status(500).json({ name, message, stack });
+  next(err);
+});
+
 module.exports = app;
